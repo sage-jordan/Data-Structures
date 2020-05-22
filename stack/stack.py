84f7a139ac5a443670b0e767785b0f84722e547a
sage-jordan <sagemjordan@gmail.com>
2020-05-21 21:58:50 -0400

    stack done

--- stack/stack.py
+++ stack/stack.py
@@ -14,22 +14,20 @@ return elements in Last In First Out order.
 
 class Stack:
     def __init__(self):
-        self.size = 0
         self.storage = []
 
     def __len__(self):
-        return self.size
+        return len(self.storage)
 
     def push(self, value):
-        self.storage += value
+        self.storage.append(value)
         return self.storage
 
     def pop(self):
-        if self.size == 0:
-            return None
-        else:
-            self.size -= 1
+        if self.__len__() > 0:
             return self.storage.pop()
+        else:
+            return None
 
 
 newStack = Stack()
