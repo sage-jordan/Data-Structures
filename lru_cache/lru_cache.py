1da80e192092c529cf2e061955ecd023b56f7535
sage-jordan <sagemjordan@gmail.com>
2020-05-26 21:58:28 -0400

    lru_cache passes

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,9 @@
+import sys
+
+sys.path.append('../doubly_linked_list')
+
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +12,14 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.max_nodes = limit
+        self.current_nodes = 0
+
+        self.dll = DoublyLinkedList()
+        self.dict = {}
+    
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +28,21 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        # return None if key-value pair doesn't exist in cache
+        if key not in self.dict:
+            return None
+        # if exists iterate over dll
+        node = self.dll.head
+        while node is not None:
+            if key == node.value[0]:
+                self.dll.move_to_front(node)
+                break
+            node = node.next
+
+        # return Value
+        return self.dict[key]
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -28,6 +53,51 @@ class LRUCache:
     case that the key already exists in the cache, we simply
     want to overwrite the old value associated with the key with
     the newly-specified value.
+
+
+    self.dll.head = (key, value) Tuple
+    self.dll.head = {key: value} Dict
+    self.dll.head = [key, value] List
     """
+
     def set(self, key, value):
-        pass
+        # if key is already stored, overwrite old value
+        if key in self.dict:
+            # overwrite in dictionary
+            self.dict[key] = value
+            # overwrite in DLL
+            ## iterate accross and find node to be updated
+            node = self.dll.head
+            while node is not None:
+                # check key equality
+                if node.value[0] == key:
+                    # and update the value
+                    node.value[1] = value
+                    # move to head of dll
+                    self.dll.move_to_front(node)
+
+                    break
+
+                node = node.next
+
+        else :
+
+            # handle case where we are full
+            if self.current_nodes == self.max_nodes:
+                #delete something
+                
+                node = self.dll.tail
+                old_key = node.value[0]
+                self.dll.remove_from_tail()
+                del self.dict[old_key]
+                # self.dict.pop(old_key)  <-- This will work and will return the value 
+                self.current_nodes -= 1
+
+
+            # add to cashe
+            self.dict[key] = value
+            self.dll.add_to_head([key, value])
+            self.current_nodes += 1
+
+
+
